d0634f2323604f979083cea109097706b28cb6a6
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-13 13:52:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = %w[string]
 
   array.each do |element|
-    reversed_array = element.reverse
+    reversed_array.push(element.reverse)
   end
   reversed_array.join(" ")
 end
